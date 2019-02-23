191a3a6ae49806189efb0120fd96156880ef7eec
hewking <604910070qq.com>
2019-02-23 14:54:23 +0800

    init project for need things

---
+++ app/src/main/java/com/github/gank/vm/GankDayViewModel.kt
@@ -0,0 +1,13 @@
+package com.github.gank.vm
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @program: HGankIO
+ *
+ * @description: ${description}
+ *
+ * @author: hewking
+ *
+ * @create: 2019-02-23 14:42
+ **/
